bd7b2bb07f52b6b49aecb8e913095146c206c1be
grammargames <58265795+grammargames@users.noreply.github.com>
2020-04-20 08:11:46 +0300

    04.20

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -5,13 +5,19 @@ insert into user_role (user_id, roles)
     values (1, 'ADMIN');
     
 insert into usr (id, username, password, active, email)
-    values (2, 'teacher', '123', true, 'n@nn.ru');
+    values (2, 'Marina Lvovna', '123', true, 'n@nn.ru');
 
 insert into user_role (user_id, roles)
     values (2, 'TEACHER');
 
 insert into usr (id, username, password, active, email)
-    values (3, 'pupil', '123', true, 'n@nnn.ru');
+    values (3, 'Sonya Kulakova', '123', true, 'n@nnn.ru');
+
+insert into user_role (user_id, roles)
+    values (3, 'USER');
+    
+insert into usr (id, username, password, active, email)
+    values (3, 'Sonya Ryzhakova', '123', true, 'n@nnn.ru');
 
 insert into user_role (user_id, roles)
     values (3, 'USER');
